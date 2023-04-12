4978a6062958d0aec66a5085c836b75d13284005
Mickaelr20 <85870744+Mickaelr20@users.noreply.github.com>
2023-04-12 18:49:55 +0400

    Migration du projet en symfony 6

---
+++ src/Controller/TasksController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TaskRepository;
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Route('/tasks', name: 'task_')]
+class TasksController extends AbstractController
+{
+	function __construct(private TaskRepository $taskRepository) {
+		
+    }
+
+	#[Route('', name: 'list', methods: ['GET'])]
+	public function list()
+	{
+		return $this->render('task/list.html.twig', ['tasks' => $this->taskRepository->findAll()]);
+	}
+
+	#[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+	public function createAction(Request $request)
+	{
+		$task = new Task();
+		$form = $this->createForm(TaskType::class, $task);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$this->taskRepository->save($task);
+
+			$this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+			return $this->redirectToRoute('task_list');
+		}
+
+		return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+	}
+	
+	#[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function editAction(Task $task, Request $request)
+    {
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+			$this->taskRepository->save($task);
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
+
+	#[Route('/{id}/toggle', name: 'toggle', methods: ['GET', 'POST'])]
+    public function toggleTaskAction(Task $task)
+    {
+        $task->toggle(!$task->isDone());
+		$this->taskRepository->save($task);
+
+        $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+
+        return $this->redirectToRoute('task_list');
+    }
+
+	#[Route('/{id}/delete', name: 'delete', methods: ['GET', 'POST'])]
+    public function deleteTaskAction(Task $task)
+    {
+        $this->taskRepository->remove($task);
+
+        $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        return $this->redirectToRoute('task_list');
+    }
+}
---
+++ src/Controller/UsersController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+#[Route('/users', name: 'user_')]
+class UsersController extends AbstractController
+{
+	function __construct(private UserRepository $userRepository) {
+		
+    }
+
+	#[Route('', name: 'list', methods: ['GET'])]
+    public function list()
+    {
+        return $this->render('user/list.html.twig', ['users' => $this->userRepository->findAll()]);
+    }
+
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function createAction(Request $request, UserPasswordHasherInterface $passwordHasher)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $this->userRepository->save($user);
+
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function editAction(User $user, Request $request, UserPasswordHasherInterface $passwordHasher)
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->userRepository->save($user);
+
+            $this->addFlash('success', "L'utilisateur a bien été modifié");
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+}
